2feb533751a0f0601a30382bbcea479bb651bdda
hoanghai31299 <hoanghai31299@gmail.com>
2020-11-30 13:47:04 +0700

    Init Branch

--- src/modules/users/model.ts
+++ src/modules/users/model.ts
@@ -1,18 +1,16 @@
 import { ModificationNote } from "../common/model";
 
 export interface IUser {
-	_id?: String;
-	name: {
-		firstName: String;
-		lastName: String;
-	};
-	email: String;
-	password: String;
-	resetPasswordToken: String;
-	resetPasswordExpires: Date;
-	phoneNumber: String;
-	gender: String;
-	isVerified?: Boolean;
-	isDeleted?: Boolean;
-	modificationNotes: ModificationNote[]
+  _id?: String;
+  name: {
+    firstName: String;
+    lastName: String;
+  };
+  email: String;
+  password: String;
+  phoneNumber: String;
+  gender: Boolean;
+  isVerified?: Boolean;
+  isDeleted?: Boolean;
+  modificationNotes: ModificationNote[];
 }
--- src/modules/users/schema.ts
+++ src/modules/users/schema.ts
@@ -1,37 +1,75 @@
-import * as mongoose from 'mongoose';
-import { ModificationNote } from '../common/model';
-import joi = require("joi");
-import { date } from 'joi';
-
+import * as mongoose from "mongoose";
+import { ModificationNote } from "../common/model";
+import * as uuid from ""
 const Schema = mongoose.Schema;
 
 const User = new Schema({
-	name: {
-		type: {
-			firstName: String,
-			lastName: String,
-		}
-	},
-	email: String,
-	password: String,
-	resetPasswordToken:{ 
-		type: String,
-		default: ""
-	},
-	  resetPasswordExpires:
-	  { type: Date, 
-		default: Date.now() },
-	phoneNumber: String,
-	gender: String,
-	isVerified:{
-		type: Boolean,
-		default: false
-	},
-	isDeleted: {
-		type: Boolean,
-		default: false
-	},
-	modificationNotes: [ModificationNote]
+  name: {
+    type: {
+      firstName: String,
+      lastName: String,
+    },
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true
+  },
+  hashed_password: {
+    type: String,
+    required: true
+  },
+  phoneNumber: String,
+  gender: Boolean,
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    enum: [0,1],
+    default: 0
+  },
+  history: {
+    type: Array,
+    default: []
+  },
+  modificationNotes: [ModificationNote],
 });
 
-export default mongoose.model('users', User);
+
+User.virtual("password")
+.set(function(password){
+  this._password = password;
+  this.salt = uuid();
+  this.hashed_password = this.encryptPassword(password);
+})
+.get(function(){
+  return this.hashed_password;
+})
+
+User.methods = {
+  encryptPassword: function(password){
+    if(!password) return "";
+    try{
+      return crypto.createHmac("sha1",this.salt)
+                    .update(password)
+                    .digest('hex');
+    }catch(err){
+      return ""
+    }
+  },
+  authenticate: function(plainText){
+      return this.encryptPassword(plainText) === this.hashed_password;
+  }
+}
+
+export default mongoose.model("users", User);
