cd4098c91e9a7d5097468dea1414cd8d92d16d92
thuanho2610 <thuanho2610@gmail.com>
2020-12-03 11:43:22 +0700

    fix conflict

--- src/config/app.ts
+++ src/config/app.ts
@@ -8,7 +8,7 @@ import { CommonRoutes } from "../routes/common_routes";
 import { ProductRoutes } from "../routes/product_routes";
 
 class App {
-  
+
   public app: express.Application;
   // public mongoUrl: string = 'mongodb://localhost/' + environment.getDBName();
   public mongoUrl: string = "mongodb://localhost/" + environment.getDBName();
@@ -16,12 +16,14 @@ class App {
   private test_routes: UserRoutes = new UserRoutes();
   private common_routes: CommonRoutes = new CommonRoutes();
   private auth_route: AuthRoute = new AuthRoute();
+  private product_routes: ProductRoutes = new ProductRoutes();
   constructor() {
     this.app = express();
     this.config();
 
     this.mongoSetup();
     this.auth_route.route(this.app);
+    this.product_routes.route(this.app);
     this.test_routes.route(this.app);
     this.common_routes.route(this.app);
   }
