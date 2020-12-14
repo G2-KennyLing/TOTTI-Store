624ff14289ecaf75aa1a642fd6c44a2b7c9bd037
hoanghai31299 <hoanghai31299@gmail.com>
2020-12-14 15:20:41 +0700

    recreate orders schema

--- src/modules/orders/model.ts
+++ src/modules/orders/model.ts
@@ -1,16 +1,17 @@
 import { ModificationNote } from "../common/model";
 
 export interface IOrder {
-	_id?: String;
-	customer_id: Number;
-	status: String;
-	order_date: Date;
-	discount_code: String;
-	store_id: Number;
-	staff_id: Number;
-	order_items: [{type: Schema.Types.product_id,
-			ref: "products"
-		}]
-		
-	
+  _id?: String;
+  customer_id: Number;
+  status: String;
+  order_date: Date;
+  discount_code: String;
+  store_id: String;
+  staff_id: String;
+  order_items: [
+    {
+      product_id: String;
+      quantity: Number;
+    }
+  ];
 }
--- src/modules/orders/schema.ts
+++ src/modules/orders/schema.ts
@@ -1,19 +1,27 @@
-import { date } from 'joi';
-import * as mongoose from 'mongoose';
-import { ModificationNote } from '../common/model';
+import { date } from "joi";
+import * as mongoose from "mongoose";
+import { ModificationNote } from "../common/model";
 
 const Schema = mongoose.Schema;
 
 const Orders = new Schema({
-	customer_id : Number,
-	status: String,
-	order_date: Date,
-	ship_date: Date,
-	discount_code: String,
-	store_id: Number,
-	staff_id: Number,
-	order_items: [{ type: Schema.Types.product_id, ref: "products" }],
-	modificationNote: [ModificationNote]
+  customer_id: String,
+  status: String,
+  order_date: Date,
+  shipping_address: String,
+  billing_address: String,
+  payment_method: {
+    type: String,
+    enum: ["Paypal", "VNPay", "Visa"],
+    required: true,
+  },
+  ship_date: Date,
+  discount_code: String,
+  store_id: String,
+  staff_id: String,
+  order_items: [{ type: Schema.Types.ObjectId, ref: "products" }],
+  //@ts-ignore
+  modificationNote: [ModificationNote],
 });
 
-export default mongoose.model('orders', Orders)
+export default mongoose.model("orders", Orders);
