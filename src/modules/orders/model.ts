44dd65dd0412c282059d8b7b87fdaf5dfb1b5a72
G2-KennyLing <63084282+G2-KennyLing@users.noreply.github.com>
2020-12-21 15:53:31 +0700

    fix bug

--- src/modules/orders/model.ts
+++ src/modules/orders/model.ts
@@ -1,28 +1,22 @@
 import { ModificationNote } from "../common/model";
 
-enum productStatus {
+enum productStatus{
 	PEDING = 0,
 	DELIVERING,
 	DELIVERED
 }
 export interface IOrder {
-  _id?: String;
-  customer_id?: String;
-  status: productStatus;
-  order_date: Date;
-  name: {
-    firstName: String;
-    lastName: String;
-  };
-  payment?: Object;
-  address: String;
-  phone_number: String;
-  payment_method: Number;
-  discount_code?: String;
-  order_items: [
-    {
-      product_id: String;
-      quantity: Number;
-    }
-  ];
+	_id?: String;
+	customer_id: String;
+	status?: productStatus;
+	order_date: Date;
+	discount_code?: String;
+	store_id: Number;
+	staff_id: Number;
+	order_items: [{
+		product_id:String,
+		quantity: Number
+	}]
+		
+	
 }
